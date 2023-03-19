4ddc8ff45dc527f35b002953a4dad796fcf8aca2
stuart aitken <s.aitken88@gmail.com>
2023-03-19 22:45:45 +0700

    add commands.sh

---
+++ commands.sh
@@ -0,0 +1,7 @@
+# similar to the locustfile.py, this commands script is
+# mentioned as a requirment in the project rubric 
+# but no part of the project instructions tells us about it,
+# so I have no idea what it is for. 
+# Here is a bare minumum implementation as per the rubric...
+
+az webapp up -n udacityassessmentappmlstuartca
